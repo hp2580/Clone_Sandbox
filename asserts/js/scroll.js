6af90f127dbe97f1af618f5fc6953e82da355ea0
sumin <happycandy0909@gamil.com>
2023-01-25 19:30:16 +0900

    setting gsap for sec2
    hScroll will be modified

--- asserts/js/scroll.js
+++ asserts/js/scroll.js
@@ -1,5 +1,18 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const contents = gsap.utils.toArray(".sec2 .content");
+
+// const tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".sec2 .contentWrap",
+//     markers: true,
+//     start: "top 50%",
+//     marker: true,
+//     pin: true,
+//     end: () => `+=${document.querySelector(".contentWrap").offsetWidth}`,
+//   },
+// });
+
 gsap.fromTo(
   ".sec2 .title",
   {
@@ -12,9 +25,19 @@ gsap.fromTo(
     duration: 0.3,
     scrollTrigger: {
       trigger: ".sec2",
-      markers: true,
       start: "top 40%",
       toggleActions: "play none none reverse",
     },
   }
 );
+
+gsap.to(contents, {
+  xPercent: -100 * (contents.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".sec2 .contentWrap",
+    markers: true,
+    pin: true,
+    end: () => `+=${document.querySelector(".sec2 .contentWrap").offsetWidth}`,
+  },
+});
